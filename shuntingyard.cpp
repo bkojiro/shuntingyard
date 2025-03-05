50b31846117a089aca5190fbc26a2326146d7cf8
U-BSD\419236 <419236@SHS-8YBF6X3.beaverton.k12.or.us>
2025-03-05 11:49:11 -0800

    figure out whats wrong w postfix

--- shuntingyard.cpp
+++ shuntingyard.cpp
@@ -6,10 +6,12 @@ using namespace std;
 
 bool isValid(char c);
 void push(Node* &stack, char c);
+void push(Node* &stack, Node* branch);
 char pop(Node* &stack);
 char peek(Node* stack);
 void enqueue(Node* &queue, char c);
 char dequeue(Node* &queue);
+void printPost(Node* tree);
 
 int main() {
   Node* queue = new Node(0);
@@ -35,25 +37,65 @@ int main() {
   while (stack->getNext() != NULL) { //if all inputs are pushed/enqueued, push rest of queue
     enqueue(queue, pop(stack));
   }
+
+  /*
+  //postfix
+  Node* temp = queue;
+  while (temp->getNext() != NULL) {
+    cout << temp->getNext()->getData();
+    temp = temp->getNext();
+  }*/
   
   //CONVERT TO EXPRESSION TREE
   Node* tree = new Node(0); //STACK
-  while (queue != NULL) {
-    if (queue->getData() < 58 && queue->getData() > 48) {
-      push(tree, dequeue(queue));
-    } else if (isValid(queue->getData())) {
-      if (queue->getData() == 94) { //unary operator
-	
-      } else { //binary operators
-	Node* temp = new Node(dequeue(queue));
-	temp->setLeft(pop(tree));
-	temp->setRight(pop(tree));
-	push(tree, temp);
+  while (queue->getNext() != NULL) {
+    char a = dequeue(queue);
+    if (a < 58 && a > 48) {
+      push(tree, new Node(a));
+    } else {
+      Node* temp = new Node(a);
+      temp->setRight(new Node(pop(tree)));
+      if (a != 94) { //binary operator
+	temp->setLeft(new Node(pop(tree)));
       }
+      push(tree, temp);
+      
     }
   }
+  delete queue;
+  delete stack;
+  cout << tree->getNext()->getLeft()->getRight()->getData();
   
+  //SET UP OUTPUT METHOD
+  bool active = true;
+  while (active) {
+    cout << "Enter INFIX, POSTFIX, or PREFIX to view output, or QUIT to exit" << endl << "> ";
+    char outputAction[10];
+    cin.get(outputAction, 11);
+    cin.ignore();
+    if (strcmp(outputAction, "INFIX") == 0) {
+      //output infix notation
+    } else if (strcmp(outputAction, "POSTFIX") == 0) {
+      //output postfix notation
+      printPost(tree->getNext());
+    } else if (strcmp(outputAction, "PREFIX") == 0) {
+      //output prefix notation
+    } else if (strcmp(outputAction, "QUIT") == 0) {
+      //terminate program
+      active = false;
+    }
+  }
   return 0;
+}
+
+void printPost(Node* current) {
+  if (current->getLeft() != NULL) {
+    printPost(current->getLeft());
+  }
+  if (current->getRight() != NULL) {
+    printPost(current->getRight());
+  }
+  cout << current->getData();
 }
 
 bool isValid(char c) {
@@ -68,10 +110,14 @@ bool isValid(char c) {
   return false;
 }
 
-void treePush(Node* &tree, Node* branch) {
-  //DO THIS
+void push(Node* &stack, Node* node) {
+  Node* current = stack;
+  while (current->getNext() != NULL) {
+    current = current->getNext();
+  }
+  current->setNext(node);
 }
- 
+
 void push(Node* &stack, char c) { //create node with value c
   Node* temp = new Node(c);
   Node* current = stack;
